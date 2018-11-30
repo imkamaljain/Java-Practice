d35ee35867c8a52537df9c08bd2cbd9102238588
Kamal Jain <43504300+kamaljain1423@users.noreply.github.com>
2018-11-30 23:59:58 +0530

    Update Select.java

--- JDBC Connectivity/Select.java
+++ JDBC Connectivity/Select.java
@@ -5,12 +5,12 @@ public static void main(String arg[])
 {
 	try
 	{
-//		Class.forName("com.mysql.jdbc.Driver");
-//		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/employee","root","root");
+		//Class.forName("com.mysql.jdbc.Driver");
+		//Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/employee","root","root");
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		Connection con=DriverManager.getConnection("jdbc:oracle:thin:kj/kj@localhost");
 		Statement stmt=con.createStatement();
-		ResultSet rs=stmt.executeQuery("create table java(id number,name varchar2(50),marks number)");
+		ResultSet rs=stmt.executeQuery("select * from java");
 		while(rs.next())
 		{
 			System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3));
